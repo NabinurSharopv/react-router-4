58399238b2b65c6ec4168bbeb7abbb9b464629f1
nabinur <nabinursh123@gmail.com>
2025-11-21 15:04:55 +0500

    nabinur

--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -2,22 +2,23 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
+  const [post, setPost] = useState([]);
 
   useEffect(() => {
-    axios.get("https://dummyjson.com/products?limit=7")
-      .then(res => setProducts(res.data.products))
-      .catch(err => console.error(err));
+    axios.get("https://dummyjson.com/products")
+      .then((res) => {
+        setPost(res.data.products); // MUHIM JOY !!!
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div>
-      {products.map(p => (
-        <div key={p.id}>
-          <h3>{p.title}</h3>
-          <p>{p.description}</p>
-          <strong>Price: ${p.price}</strong>
-          <hr />
+    <div className="flex gap-9 max-w-1000 flex-wrap items-center justify-center mt-3">
+      {post.map((item) => (
+        <div key={item.id} className="border rounded-[10px] p-3">
+          <img src={item.images[0]} width={200} />
+           <h1>{item.price}</h1>
+          <p>{item.title}</p>
         </div>
       ))}
     </div>
